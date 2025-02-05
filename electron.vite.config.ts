9c5c9eb17fa5dfb89a56505772762be828a2f03f
deuscx <1475453714@qq.com>
2025-02-06 07:22:30 +0800

    first commit

---
+++ electron.vite.config.ts
@@ -0,0 +1,101 @@
+import path, { resolve } from 'node:path'
+import Vue from '@vitejs/plugin-vue'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import VueMacros from 'unplugin-vue-macros/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import VueRouter from 'unplugin-vue-router/vite'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        // external: EXTERNAL_PACKAGES,
+        input: {
+          index: path.resolve(__dirname, 'electron/main/index.ts'),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '~': resolve('./electron/main/src'),
+      },
+    },
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        // external: EXTERNAL_PACKAGES,
+        input: {
+          index: path.resolve(__dirname, 'electron/preload/index.ts'),
+        },
+      },
+    },
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    root: path.resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '~': resolve('src'),
+      },
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/index.html'),
+        },
+      },
+    },
+    plugins: [
+      VueMacros({
+        defineOptions: false,
+        defineModels: false,
+        plugins: {
+          vue: Vue({
+            script: {
+              propsDestructure: true,
+              defineModel: true,
+            },
+          }),
+        },
+      }),
+
+      // https://github.com/posva/unplugin-vue-router
+      VueRouter(),
+
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          '@vueuse/core',
+          VueRouterAutoImports,
+          {
+            // add any other imports you were relying on
+            'vue-router/auto': ['useLink'],
+          },
+        ],
+        dts: true,
+        dirs: [
+          // './src/composables',
+          'composables',
+        ],
+        vueTemplate: true,
+      }),
+
+      // https://github.com/antfu/vite-plugin-components
+      Components({
+        dts: true,
+        dirs: ['components'],
+        deep: true,
+      }),
+
+      // https://github.com/antfu/unocss
+      // see uno.config.ts for config
+      UnoCSS(),
+    ],
+  },
+})
---
+++ src/components/Versions.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const versions = reactive({
+  electron: '31.0.2',
+  chrome: '120.0.6099.128',
+  node: '20.14.8',
+})
+
+</script>
+
+<template>
+  <ul class="versions">
+    <li class="electron-version">Electron v{{ versions.electron }}</li>
+    <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
+    <li class="node-version">Node v{{ versions.node }}</li>
+  </ul>
+</template>
